f22bbcc4c8886ca42dbb9ef73f62a5e5c69c880f
Joy Sameza <samgaming500@gmail.com>
2023-05-19 06:19:10 +0100

    next commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const port = parseInt(process.env.PORT) || 3000;
+const { join } = require("path");
+const express = require("express"),
+  app = express();
+
+//Middlewares
+app.use(express.static(join(__dirname, "client")));
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    res.end("index.html");
+});
+
+app.listen(port, () => console.log("Listening on port " + port));
